8e97268d7e9d92ccf0911b56b439d41ab67745af
Angela Wolff <warriorwomenblog@gmail.com>
2019-01-15 21:52:30 +0000

    Using url-loader for loading images

--- src/index.js
+++ src/index.js
@@ -2,11 +2,15 @@ import nav from "./nav";
 import { footer } from "./footer";
 import makeButton from "./button";
 import { makeColorStyle } from "./button-styles";
+import makeImage from './image.js';
+import iconUrl from '../icon.jpg'
 
 import css from "./footer.css";
 import buttonStyles from "./button.css";
 
+const icon = makeImage(iconUrl)
 const button = makeButton("Yay! A Button!");
 button.style = makeColorStyle("cyan");
 document.body.appendChild(button);
+document.body.appendChild(icon);
 document.body.appendChild(footer);
